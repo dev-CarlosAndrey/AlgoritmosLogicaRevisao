d35175573178803c870dd2e64fe6106812bc3308
dev-CarlosAndrey <andreybezerra.info@gmail.com>
2025-03-12 09:18:15 -0300

    alteracoes

--- questao12.c
+++ questao12.c
@@ -2,6 +2,15 @@
 
 int main(void)
 {
+    double PI = 3.141592;
+    double raio, area;
+
+    printf("Digite o valor do raio do circulo: ");
+    scanf("%lf", &raio);
+
+    area = PI * raio * raio;
+
+    printf("A area do circulo e: %.2lf \n", area);
     
     return 0;
 }
